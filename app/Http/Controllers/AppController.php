57be562757b6e5c63569f0d3392151ac9907421b
esanasar <ehsan.asarzade@gmail.com>
2020-06-25 05:59:00 +0430

    swagger added

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -5,16 +5,69 @@ namespace App\Http\Controllers;
 use App\App;
 use App\Http\Resources\AppResource;
 use Illuminate\Http\Request;
+use http\Env\Response;
 
 class AppController extends Controller
 {
     /**
+     *
+     * @param Request $request
+     * @return Response|string
+     */
+
+
+    /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
+     */
+
+    /**
+     * @SWG\Get(
+     *     path="api/apps",
+     *     description="Return all apps",
+     *     @SWG\Parameter(
+     *         name="name",
+     *         in="query",
+     *         type="string",
+     *         description="app name",
+     *         required=true,
+     *     ),
+     *     @SWG\Parameter(
+     *         name="description",
+     *         in="query",
+     *         type="string",
+     *         description="app description",
+     *         required=true,
+     *     ),
+     *     @SWG\Parameter(
+     *         name="icon",
+     *         in="query",
+     *         type="string",
+     *         description="app icon",
+     *         required=true,
+     *     ),
+     *     @SWG\Parameter(
+     *         name="creator",
+     *         in="query",
+     *         type="string",
+     *         description="app creator",
+     *         required=true,
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *     ),
+     *     @SWG\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *
+     * )
      */
     public function index()
     {
+        abort_if(Gate::denies('project_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
         return AppResource::collection(App::get());
     }
 
@@ -22,6 +75,53 @@ class AppController extends Controller
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * @SWG\Post(
+     *     path="api/apps",
+     *     description="add new record to app table",
+     *     @SWG\Parameter(
+     *         name="name",
+     *         in="query",
+     *         type="string",
+     *         description="app name",
+     *         required=true,
+     *     ),
+     *     @SWG\Parameter(
+     *         name="description",
+     *         in="query",
+     *         type="string",
+     *         description="app description",
+     *         required=true,
+     *     ),
+     *     @SWG\Parameter(
+     *         name="icon",
+     *         in="query",
+     *         type="string",
+     *         description="app icon",
+     *         required=true,
+     *     ),
+     *     @SWG\Parameter(
+     *         name="creator",
+     *         in="query",
+     *         type="string",
+     *         description="app creator",
+     *         required=true,
+     *     ),
+     *     @SWG\Response(
+     *         response=201,
+     *         description="OK",
+     *     ),
+     *     @SWG\Response(
+     *         response=400,
+     *         description="Bad request",
+     *     ),
+     *     @SWG\Response(
+     *         response=403,
+     *         description="Forbidden",
+     *     )
+     * )
      */
 
     public function store(Request $request)
@@ -32,20 +132,75 @@ class AppController extends Controller
             'icon' => 'required|url',
             'creator' => 'required|max:50'
         ]);
+//        $appData = $request->all();
+
+//        if (empty($appData['name']) && empty($appData['description']) && empty($appData['icon']) && empty($appData['creator'])) {
+//            return new \Exception('Missing data', 404);
+//        }
 
         $app = App::create($request->all());
 
-        return new AppResource($app);
+        return (new AppResource($app))
+            ->response()
+            ->setStatusCode(Response::HTTP_CREATED);;
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int  $app
      * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * @SWG\Get(
+     *     path="api/apps/{app}",
+     *     description="show one record in app table",
+     *     @SWG\Parameter(
+     *         name="name",
+     *         in="query",
+     *         type="string",
+     *         description="app name",
+     *         required=true,
+     *     ),
+     *     @SWG\Parameter(
+     *         name="description",
+     *         in="query",
+     *         type="string",
+     *         description="app description",
+     *         required=true,
+     *     ),
+     *     @SWG\Parameter(
+     *         name="icon",
+     *         in="query",
+     *         type="string",
+     *         description="app icon",
+     *         required=true,
+     *     ),
+     *     @SWG\Parameter(
+     *         name="creator",
+     *         in="query",
+     *         type="string",
+     *         description="app creator",
+     *         required=true,
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="OK",
+     *     ),
+     *     @SWG\Response(
+     *         response=400,
+     *         description="Bad request",
+     *     ),
+     *     @SWG\Response(
+     *         response=403,
+     *         description="Forbidden",
+     *     )
+     * )
      */
     public function show(App $app)
     {
+        abort_if(Gate::denies('project_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
         return new AppResource($app);
 
     }
@@ -55,8 +210,59 @@ class AppController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  int  $app
      * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * @SWG\Put(
+     *     path="api/apps/{app}",
+     *     description="update record in app table",
+     *     @SWG\Parameter(
+     *         name="name",
+     *         in="query",
+     *         type="string",
+     *         description="app name",
+     *         required=true,
+     *     ),
+     *     @SWG\Parameter(
+     *         name="description",
+     *         in="query",
+     *         type="string",
+     *         description="app description",
+     *         required=true,
+     *     ),
+     *     @SWG\Parameter(
+     *         name="icon",
+     *         in="query",
+     *         type="string",
+     *         description="app icon",
+     *         required=true,
+     *     ),
+     *     @SWG\Parameter(
+     *         name="creator",
+     *         in="query",
+     *         type="string",
+     *         description="app creator",
+     *         required=true,
+     *     ),
+     *     @SWG\Response(
+     *         response=202,
+     *         description="OK",
+     *     ),
+     *     @SWG\Response(
+     *         response=400,
+     *         description="Bad request",
+     *     ),
+     *     @SWG\Response(
+     *         response=403,
+     *         description="Forbidden",
+     *     ),
+     *     @SWG\Response(
+     *         response=404,
+     *         description="Not found",
+     *     )
+     * )
      */
     public function update(Request $request, App $app)
     {
@@ -69,19 +275,70 @@ class AppController extends Controller
 
         $app->update($request->all());
 
-        return new AppResource($app);
+        return (new AppResource($app))
+            ->response()
+            ->setStatusCode(Response::HTTP_CREATED);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int  $app
      * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * @SWG\Delete(
+     *     path="api/apps/{app}",
+     *     description="delete record from app table",
+     *     @SWG\Parameter(
+     *         name="name",
+     *         in="query",
+     *         type="string",
+     *         description="app name",
+     *         required=true,
+     *     ),
+     *     @SWG\Parameter(
+     *         name="description",
+     *         in="query",
+     *         type="string",
+     *         description="app description",
+     *         required=true,
+     *     ),
+     *     @SWG\Parameter(
+     *         name="icon",
+     *         in="query",
+     *         type="string",
+     *         description="app icon",
+     *         required=true,
+     *     ),
+     *     @SWG\Parameter(
+     *         name="creator",
+     *         in="query",
+     *         type="string",
+     *         description="app creator",
+     *         required=true,
+     *     ),
+     *     @SWG\Response(
+     *         response=204,
+     *         description="OK",
+     *     ),
+     *     @SWG\Response(
+     *         response=403,
+     *         description="Forbidden",
+     *     ),
+     *     @SWG\Response(
+     *         response=404,
+     *         description="Not found",
+     *     )
+     * )
      */
     public function destroy(App $app)
     {
+        abort_if(Gate::denies('project_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
         $app->delete();
 
-        return response('massege' , 'deleted');
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,60 +1,61 @@
-<?php
+<!-- HTML for static distribution bundle build -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Swagger UI</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Source+Code+Pro:300,600|Titillium+Web:400,600,700" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/swagger/swagger-ui.css" >
+    <link rel="icon" type="image/png" href="/swagger/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="/swagger/favicon-16x16.png" sizes="16x16" />
+    <style>
+      html
+      {
+        box-sizing: border-box;
+        overflow: -moz-scrollbars-vertical;
+        overflow-y: scroll;
+      }
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
+      *,
+      *:before,
+      *:after
+      {
+        box-sizing: inherit;
+      }
 
-define('LARAVEL_START', microtime(true));
+      body
+      {
+        margin:0;
+        background: #fafafa;
+      }
+    </style>
+  </head>
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels great to relax.
-|
-*/
+  <body>
+    <div id="swagger-ui"></div>
 
-require __DIR__.'/../vendor/autoload.php';
+    <script src="/swagger/swagger-ui-bundle.js"> </script>
+    <script src="/swagger/swagger-ui-standalone-preset.js"> </script>
+    <script>
+    window.onload = function() {
 
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
+      // Build a system
+      const ui = SwaggerUIBundle({
+        url: url = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + "/swagger/swagger.json",
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+          SwaggerUIBundle.presets.apis,
+          SwaggerUIStandalonePreset
+        ],
+        plugins: [
+          SwaggerUIBundle.plugins.DownloadUrl
+        ],
+        layout: "StandaloneLayout"
+      })
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+      window.ui = ui
+    }
+  </script>
+  </body>
+</html>
